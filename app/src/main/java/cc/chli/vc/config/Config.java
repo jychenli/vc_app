830b7b6b30ec6b0c8caca667440481df31f407e4
jychenli <jychenli@gmail.com>
2017-07-16 11:28:23 +0800

    config 文件

---
+++ app/src/main/java/cc/chli/vc/config/Config.java
@@ -0,0 +1,9 @@
+package cc.chli.vc.config;
+
+/**
+ * Created by LYB on 2016/10/24.
+ */
+public class Config {
+    public static boolean Debug = true;
+    public static boolean useCanary = Debug;
+}
